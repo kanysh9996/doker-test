a844cb87c11d220055a0a0567686ec148245bd9c
kanysh9996 <zholonovakanykei@gmail.com>
2025-02-24 14:42:15 +0600

    gdg

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -138,8 +138,8 @@ MODELTRANSLATION_LANGUAGES = ('en', 'ru')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-SECRET_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
